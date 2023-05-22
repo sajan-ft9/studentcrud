c925a1b663393f3373b0376b119134e5ead6c25d
sajan <sajankhad2@gmail.com>
2023-05-22 14:15:11 +0545

    first

---
+++ resources/views/student/create.blade.php
@@ -0,0 +1,25 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-12">
+            <div class="card">
+                <div class="card-header text-center">
+                    <h2>Create Student Profile</h2>
+                </div>
+
+                <div class="card-body">
+                    @if (session('status'))
+                        <div class="alert alert-success" role="alert">
+                            {{ session('status') }}
+                        </div>
+                    @endif
+
+                    
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
